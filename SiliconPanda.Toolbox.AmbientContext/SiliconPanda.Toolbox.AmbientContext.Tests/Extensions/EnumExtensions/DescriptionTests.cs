14bd5e49d4944d55ade0e3e841ba1f56e27c52d9
Matthew Worthington <worthingtown@gmail.com>
2017-11-24 20:00:59 +0000

    Initial commit- previous commit failed

---
+++ SiliconPanda.Toolbox.AmbientContext/SiliconPanda.Toolbox.AmbientContext.Tests/Extensions/EnumExtensions/DescriptionTests.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NUnit.Framework;
+using SiliconPanda.Toolbox.AmbientContext.Extensions;
+
+namespace SiliconPanda.Toolbox.AmbientContext.Tests.Extensions.EnumExtensions
+{
+    [TestFixture]
+    public class DescriptionTests
+    {
+        public class EnumExtensions
+        {
+            [TestCase(TestEnum.Test, ExpectedResult = "Test")]
+            [TestCase(TestEnum.Test, ExpectedResult = "Another Test")]
+            public string EnumShouldReturnDescription(TestEnum value)
+            {
+                return value.Description();
+            }
+        }
+
+        public enum TestEnum
+        {
+            Test,
+            [Display(Description = "Another Test")]
+            AnotherTest
+        }
+    }
+}
---
+++ SiliconPanda.Toolbox.AmbientContext/SiliconPanda.Toolbox.AmbientContext/Extensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace SiliconPanda.Toolbox.AmbientContext.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string Description(this Enum value)
+        {
+            return !(value.GetType()
+                .GetField(value.ToString())
+                .GetCustomAttributes(typeof(DisplayAttribute), false)
+                .SingleOrDefault() is DisplayAttribute attribute) ? value.ToString() : attribute.Description;
+        }
+    }
+}
